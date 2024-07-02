ecd413b682c55aa4f0ddd21a244266cfff0153d4
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-07-02 18:30:10 -0300

    feat: add methods

--- src/main/java/com/br/orderms/service/OrderService.java
+++ src/main/java/com/br/orderms/service/OrderService.java
@@ -1,10 +1,15 @@
 package com.br.orderms.service;
 
 import com.br.orderms.dto.OrderCreatedEvent;
+import com.br.orderms.dto.api.OrderResponse;
 import com.br.orderms.entity.OrderEntity;
 import com.br.orderms.factory.OrderFactoryInterface;
 import com.br.orderms.repository.OrderRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService implements OrderServiceInterface {
@@ -15,6 +20,12 @@ public class OrderService implements OrderServiceInterface {
     public OrderService(OrderRepository orderRepository, OrderFactoryInterface orderFactory) {
         this.orderRepository = orderRepository;
         this.orderFactory = orderFactory;
+    }
+
+    public Page<OrderResponse> findAllByCustomerId(Long customerId, PageRequest pageRequest) {
+        var orders = orderRepository.findAllByCustomerId(customerId, pageRequest);
+
+        return orders.map(OrderResponse::fromEntity);
     }
 
     @Override
--- src/main/java/com/br/orderms/service/OrderServiceInterface.java
+++ src/main/java/com/br/orderms/service/OrderServiceInterface.java
@@ -1,7 +1,11 @@
 package com.br.orderms.service;
 
 import com.br.orderms.dto.OrderCreatedEvent;
+import com.br.orderms.dto.api.OrderResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 public interface OrderServiceInterface {
+    public Page<OrderResponse> findAllByCustomerId(Long customerId, PageRequest pageRequest);
     public void save(OrderCreatedEvent event);
 }
