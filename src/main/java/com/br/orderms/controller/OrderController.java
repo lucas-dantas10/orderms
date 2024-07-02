d40c53375b04c2747db2af91dfb85b3885117e3b
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-07-02 18:24:20 -0300

    feat: add endpoint to list orders customer

---
+++ src/main/java/com/br/orderms/controller/OrderController.java
@@ -0,0 +1,40 @@
+package com.br.orderms.controller;
+
+import com.br.orderms.dto.api.ApiResponse;
+import com.br.orderms.dto.api.OrderResponse;
+import com.br.orderms.dto.api.PaginationResponse;
+import com.br.orderms.entity.OrderEntity;
+import com.br.orderms.service.OrderServiceInterface;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class OrderController {
+
+    private final OrderServiceInterface orderService;
+
+    public OrderController(OrderServiceInterface orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/customers/{customerId}/orders")
+    public ResponseEntity<ApiResponse<OrderResponse>> listOrders(
+            @PathVariable("customerId") Long customerId,
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize
+    ) {
+
+        var pageResponse = orderService.findAllByCustomerId(customerId, PageRequest.of(page, pageSize));
+
+        return ResponseEntity.ok(new ApiResponse<>(
+                pageResponse.getContent(),
+                PaginationResponse.fromPage(pageResponse)
+        ));
+    }
+}
