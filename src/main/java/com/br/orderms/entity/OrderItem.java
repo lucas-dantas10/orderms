f5da4444f9669c2125daac9e922b94869aefd348
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-07-02 16:38:17 -0300

    feat: add 2 constructors

--- src/main/java/com/br/orderms/entity/OrderItem.java
+++ src/main/java/com/br/orderms/entity/OrderItem.java
@@ -17,6 +17,16 @@ public class OrderItem {
     public OrderItem() {
     }
 
+    public OrderItem(
+            String product,
+            Integer quantity,
+            BigDecimal price
+    ) {
+        this.product = product;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
     public String getProduct() {
         return product;
     }
