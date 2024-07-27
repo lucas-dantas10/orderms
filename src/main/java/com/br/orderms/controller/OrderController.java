44d9cb707ccdd90a703870a0113d25cba00c21f0
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-07-27 11:22:33 -0300

    fix: remove unnecessary import

--- src/main/java/com/br/orderms/controller/OrderController.java
+++ src/main/java/com/br/orderms/controller/OrderController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
